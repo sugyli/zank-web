ed59b5b5e108c6d9105a594f583adda935d3eb10
Seven <lovevipdsw@outlook.com>
2016-07-31 23:42:06 +0800

    test

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    var_dump(Zank\Model\User::select('show tables'));
+    var_dump(Zank\Model\User::class);
 });
