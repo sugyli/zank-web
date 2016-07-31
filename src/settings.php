2fc3044cc4db58fba8cd01ff4a204afec70dcf3c
Seven <lovevipdsw@outlook.com>
2016-08-01 01:02:45 +0800

    修改前缀

--- src/settings.php
+++ src/settings.php
@@ -17,7 +17,7 @@ return [
             'password' => '',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
+            'prefix' => 'medz_',
         ],
     ],
 ];
