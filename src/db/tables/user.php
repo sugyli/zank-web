e8f061234e98a2c9b4c56a0cc7e6c7a6c846d356
Seven <lovevipdsw@outlook.com>
2016-08-02 21:36:51 +0800

    添加新东西

--- src/db/tables/user.php
+++ src/db/tables/user.php
@@ -3,5 +3,15 @@
 use Illuminate\Database\Schema\Blueprint;
 
 return function (Blueprint $table) {
-    $table;
+    $table->increments('uid');
+    $table->string('phone');
+    $table->string('username', 100);
+    $table->bigInteger('avatar')->default(0);
+    $table->smallInteger('age')->default(0);
+    $table->smallInteger('height')->defalut(170);
+    $table->smallInteger('kg')->defalut(60);
+    $table->enum('role', ['1', '0.5', '0'])->defalut('0.5');
+    $table->primary('id');
+    $table->unique('phone');
+    $table->unique('name');
 };
