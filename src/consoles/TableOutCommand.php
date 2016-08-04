de21db2a88d27bced036a04ee3b5cc20be25788d
Seven <lovevipdsw@outlook.com>
2016-08-04 23:32:07 +0800

    增加删除数据表Command

---
+++ src/consoles/TableOutCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Zank\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class TableOutCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('db:delete')
+            ->setDescription('Delete all tables.')
+            ->setHelp('This command delete all tables to database.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Delete all tables.',
+            '======================',
+            ''
+        ]);
+
+        $tablesDir = dirname(__DIR__).'/db/tables';
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->in($tablesDir)
+            ->name('*.php')
+        ;
+
+        $i = 0;
+        foreach ($finder as $file) {
+            $tableName = $file->getBasename('.php');
+            Capsule::Schema()->dropIfExists($tableName); // 删除数据库的表
+            $output->writeln('Delete table:'.$tableName.' <fg=green>OK.</>');
+            $i++;
+        }
+
+        $output->writeln(sprintf('<info>Delete table num:%d</info>', $i));
+    }
+}
