7fcb4a14c5b69af27b91ad4accaffc6b2d45f6a4
Seven <lovevipdsw@outlook.com>
2016-08-09 09:52:43 +0800

    fixer code style.

--- src/dependencies.php
+++ src/dependencies.php
@@ -6,16 +6,19 @@ use Slim\Container;
 $container = $app->getContainer();
 
 // monolog
-$container['logger'] = function (Container $c) {
+$container['logger'] = function (Container $c)
+{
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+
     return $logger;
 };
 
 // Service factory for the Eloquent ORM.
-$container['db'] = function (Container $c) {
+$container['db'] = function (Container $c)
+{
     $capsule = new \Illuminate\Database\Capsule\Manager;
     $capsule->addConnection($c->get('settings')['db']);
 
