24f6381a4b19d5ea4a60b76d32d879c628e62dc8
Seven <lovevipdsw@outlook.com>
2016-08-09 09:53:04 +0800

    修改中间件配置。

--- src/middleware.php
+++ src/middleware.php
@@ -7,15 +7,5 @@ use Psr\Http\Message\ResponseInterface as Response;
 // e.g: $app->add(new \Slim\Csrf\Guard);
 //
 
-$app->add(function (Request $request, Response $response, callable $next) {
-    $uri = $request->getUri();
-    $path = $uri->getPath();
-    if ($path != '/' && substr($path, -1) == '/') {
-        // permanently redirect paths with a trailing slash
-        // to their non-trailing counterpart
-        $uri = $uri->withPath(substr($path, 0, -1));
-        return $response->withRedirect((string) $uri, 301);
-    }
-
-    return $next($request, $response);
-});
+// Trailing slash
+$app->add(Zank\Middleware\TrailingSlash::class);
