c8abf7f887e99aebe88e0571da560166492a43d1
Seven <lovevipdsw@outlook.com>
2016-08-09 09:54:39 +0800

    添加用户注册路由。

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,15 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $this->get('db');
-    var_dump(Zank\Model\User::all());
+$app->group('/api', function() {
+    // index
+    $this->any('', function(Request $request, Response $response, array $args) {
+        var_dump($args);
+    });
+
+    // 注册
+    $this
+        ->any('/sign-up', Zank\Controller\Api\SignUp::class)
+        ->add(Zank\Middleware\InitDb::class)
+    ;
 });
