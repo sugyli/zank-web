6038c8f1c64b545587d475edbf4c9f20e6a8be92
Seven <lovevipdsw@outlook.com>
2016-08-17 15:23:37 +0800

    改变路由配置

--- src/routes.php
+++ src/routes.php
@@ -3,15 +3,33 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$app->group('/api', function() {
+$app->group('/api', function () {
     // index
-    $this->any('', function(Request $request, Response $response, array $args) {
-        var_dump($args);
+    $this->any('', function(Request $request, Response $response) {
+        $apiList = [
+            '/api/sign/' => '用户注册｜登陆',
+        ];
+        $response->withJson($apiList);
+
+        return $response;
     });
 
-    // 注册
-    $this
-        ->any('/sign-up', Zank\Controller\Api\SignUp::class)
-        ->add(Zank\Middleware\InitDb::class)
-    ;
+
+    // 用户注册｜登陆
+    $this->group('/sign', function () {
+        // 索引
+        $this->any('', function (Request $request, Response $response) {
+            $response->withJson([
+                '/api/sign/up' => '用户注册',
+            ]);
+
+            return $response;
+        });
+
+        // 注册
+        $this
+            ->any('/up', Zank\Controller\Api\Sign::class.':up')
+            ->add(Zank\Middleware\InitDb::class)
+        ;
+    });
 });
