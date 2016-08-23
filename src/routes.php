1da829709162b49c2ce7fa13a9a83dae6fc7ea5e
Seven <lovevipdsw@outlook.com>
2016-08-23 18:25:57 +0800

    修改路由

--- src/routes.php
+++ src/routes.php
@@ -18,17 +18,17 @@ $app->group('/api', function () {
     // 用户注册｜登陆
     $this->group('/sign', function () {
         // 索引
-        $this->any('', function (Request $request, Response $response) {
-            $response->withJson([
-                '/api/sign/up' => '用户注册',
-            ]);
-
-            return $response;
-        });
+        $this->any('', Zank\Controller\Api\Sign::class);
 
         // 注册
         $this
             ->any('/up', Zank\Controller\Api\Sign::class.':up')
+            ->add(Zank\Middleware\InitDb::class)
+        ;
+
+        // 登陆
+        $this
+            ->post('/in', Zank\Controller\Api\Sign::class,':in')
             ->add(Zank\Middleware\InitDb::class)
         ;
     });
