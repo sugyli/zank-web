142d73bb6ad154bf537dd2a5c550f33d868962aa
Seven <lovevipdsw@outlook.com>
2016-08-25 14:03:16 +0800

    修改command配置

--- src/db/console.php
+++ src/db/console.php
@@ -3,8 +3,12 @@
 $settings = require dirname(__DIR__).'/settings.php';
 
 try {
+
+    $settings = $settings['settings']['db'];
+    $settings = $settings['connections'][$settings['default']];
+
     $capsule = new \Illuminate\Database\Capsule\Manager;
-    $capsule->addConnection($settings['settings']['db']);
+    $capsule->addConnection($settings);
 
     $capsule->setAsGlobal();
     $capsule->bootEloquent();
