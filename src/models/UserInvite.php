a23ae3c2c3ad6773439e160909dea3101d6a79ba
Seven <lovevipdsw@outlook.com>
2016-08-25 17:29:44 +0800

    邀请码模型

---
+++ src/models/UserInvite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zank\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * 用户邀请码数据模型
+ *
+ * @author Seven Du <lovevipdsw@outlook.com>
+ **/
+class UserInvite extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'user_invite';
+
+    protected $primaryKey = 'user_invite_id';
+
+    public function scopeByInviteCode(Builder $query, $inviteCode): Builder
+    {
+        return $query->where('invite_code', $inviteCode);
+    }
+
+    public function scopeByCreateUsersId(Builder $query, $usersId): Builder
+    {
+        return $query->where('create_users_id', $usersId);
+    }
+
+    public function scopeByUseUsersId(Builder $query, $usersId): Builder
+    {
+        return $query->where('use_users_id', $usersId);
+    }
+} // END class UserInvite extends Model
