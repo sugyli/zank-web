4bbc1a8757f1916684af83ad943f5408da34c256
Seven <lovevipdsw@outlook.com>
2016-08-25 16:55:07 +0800

    规范路由器命名空间的使用

--- src/routes.php
+++ src/routes.php
@@ -20,19 +20,21 @@ $app->group('/api', function (): void
     $this->group('/sign', function (): void
     {
         // 索引
-        $this->any('', Zank\Controller\Api\Sign::class);
+        $this->any('', \Zank\Controller\Api\Sign::class);
 
         // 注册
         $this
-            ->post('/up', Zank\Controller\Api\Sign::class.':up')
-            ->add(Zank\Middleware\InitDb::class)
+            ->post('/up', \Zank\Controller\Api\Sign::class.':up')
+            ->add(\Zank\Middleware\Sign\Up\ValidateUserByUserName::class)
+            ->add(\Zank\Middleware\Sign\Up\ValidateUserByPhone::class)
+            ->add(\Zank\Middleware\InitDb::class)
         ;
 
         // 登陆
         $this
-            ->any('/in', Zank\Controller\Api\Sign::class.':in')
-            ->add(Zank\Middleware\AuthenticationUserToken::class)
-            ->add(Zank\Middleware\InitDb::class)
+            ->any('/in', \Zank\Controller\Api\Sign::class.':in')
+            ->add(\Zank\Middleware\AuthenticationUserToken::class)
+            ->add(\Zank\Middleware\InitDb::class)
         ;
     });
 });
