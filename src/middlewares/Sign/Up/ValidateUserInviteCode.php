5d78ef5a69a61a1b8eddb1c3bbcc80cefb64c396
Seven <lovevipdsw@outlook.com>
2016-08-25 17:29:38 +0800

    添加注册邀请码是非存在中间件。

---
+++ src/middlewares/Sign/Up/ValidateUserInviteCode.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Zank\Middleware\Sign\Up;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Interop\Container\ContainerInterface;
+
+/**
+ * 验证注册是使用的邀请码
+ *
+ * @author Seven Du <lovevipdsw@outlook.com>
+ **/
+class ValidateUserInviteCode
+{
+    protected $ci;
+
+    public function __construct(ContainerInterface $ci) {
+        $this->ci = $ci;
+    }
+
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $inviteCode = $request->getParsedBodyParam('invite_code');
+
+        if ($inviteCode) {
+            $invite = \Zank\Model\UserInvite::byInviteCode($inviteCode)
+                ->first()
+            ;
+
+            if (!$invite) {
+                $response = new \Zank\Common\Message($response, false, '该邀请码不存。');
+                return $response->withJson();
+            }
+        }
+
+        return $next($request, $response);
+    }
+} // END class ValidateInviteCode
