04e52a5da678e9f907a5731fe7390919fabeb8b3
Seven <lovevipdsw@outlook.com>
2016-08-25 16:27:09 +0800

    添加邀请码table迁移代码

---
+++ src/db/tables/user_invite.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+
+return function (Blueprint $table): void
+{
+    $table->increments('user_invite_id');
+    $table->string('invite_code', 255);
+    $table->integer('create_users_id');
+    $table->integer('use_users_id')->nullable()->default(null);
+    $table->softDeletes();
+
+    $table->index('invite_code');
+    $table->index('create_users_id');
+    $table->index('use_users_id');
+};
