d920b6f16c96bbff3b243fb729feb2ec3a843ffe
Seven <lovevipdsw@outlook.com>
2016-08-25 16:54:30 +0800

    修改注册中间件错误的代码

--- src/middlewares/Sign/Up/ValidateUserByPhone.php
+++ src/middlewares/Sign/Up/ValidateUserByPhone.php
@@ -27,9 +27,13 @@ class ValidateUserByPhone
             $response = new \Zank\Model\Message($response, false, '手机号码不正确。');
         }
 
-        $user = $this->ci->get('user');
-        if (!$user) {
-            $user = \Zank\Model\User::byPhone($phone)->frist();
+        // 检查是否存在注入的用户信息，如果有，获取
+        if ($this->ci->has('user')) {
+            $user = $this->ci->get('user');
+
+        // 不存在注入信息，查询信息，并注入
+        } else {
+            $user = \Zank\Model\User::byPhone($phone)->first();
             $this->ci->offsetSet('user', $user);
         }
 
@@ -42,7 +46,7 @@ class ValidateUserByPhone
 
             // 只要用户存在就不允许
             } else {
-                $response = new \Zank\Model\Message($response, false, '当前用户名或者手机号码不能注册。');
+                $response = new \Zank\Common\Message($response, false, '当前用户名或者手机号码不能注册。');
             }
 
             return $response->withJson();
