238d3781a5d26b7038dcc9c29a5e70678a9efb37
Seven <lovevipdsw@outlook.com>
2016-08-26 13:38:23 +0800

    完成登陆接口

--- src/controllers/Api/Sign.php
+++ src/controllers/Api/Sign.php
@@ -7,47 +7,13 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Zank\Controller;
 
 /**
- * 注册控制器
+ * 认证控制器
  *
  * @package default
  * @author Seven Du <lovevipdsw@outlook.com>
  **/
 class Sign extends Controller
 {
-
-    /**
-     * 注册控制器
-     *
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     * @author Seven Du <lovevipdsw@outlook.com>
-     * @homepage http://medz.cn
-     */
-    public function up(Request $request, Response $response)
-    {
-        $phone = $request->getParsedBodyParam('phone');
-        $username = $request->getParsedBodyParam('username');
-        $password = $request->getParsedBodyParam('password');
-        $invite_code = $request->getParsedBodyParam('invite_code');
-
-        $user = new \Zank\Model\User;
-        $user->phone = $phone;
-        $user->username = $username;
-        $user->hash = str_random(64);
-        $user->password = md5($user->hash.$password);
-
-        if (!$user->save()) {
-            $response = new \Zank\Common\Message($response, false, '创建用户失败！');
-
-            return $response->withJson();
-        }
-
-        var_dump($user);
-        exit;
-
-    }
-
     /**
      * 登陆控制器
      *
@@ -55,12 +21,44 @@ class Sign extends Controller
      */
     public function in(Request $request, Response $response)
     {
-        $user = $request->getParsedBodyParam('user');
+        $phone = $request->getParsedBodyParam('phone');
         $password = $request->getParsedBodyParam('password');
-        var_dump(11);exit;
-        return 123;
-        register
-        // var_dump($this->ci->demo);exit;
+
+        if ($this->ci->has('user')) {
+            $user = $this->ci->get('user');
+        } else {
+            $user = \Zank\Model\User::byPhone($phone)->first();
+        }
+
+        if ($user->password != md5($user->hash.$password)) {
+            $response = new \Zank\Common\Message($response, false, '该用户密码错误！');
+
+            return $response->withJson();
+        }
+
+        $token = new \Zank\Model\SignToken;
+        $token->token = \Zank\Model\SignToken::createToken();
+        $token->refresh_token = \Zank\Model\SignToken::createRefreshToken();
+        $token->users_id = $user->users_id;
+        $token->expires = 60 * 60 * 24; // 24小时过期
+
+        // 清除token
+        \Zank\Model\SignToken::where('token', $token->token)
+            ->orWhere('refresh_token', $token->refresh_token)
+            ->orWhere('users_id', $user->users_id)
+            ->delete()
+        ;
+
+        if (!$token->save()) {
+            $response = new \Zank\Common\Message($response, false, '登陆失败！');
+
+            return $response->withJson();
+        }
+
+        $response = new \Zank\Common\Message($response, true, '登陆成功！', $token);
+
+        return $response->withJson();
+
     }
 
     public function setpResisterBase(Request $request, Response $response)
@@ -71,12 +69,18 @@ class Sign extends Controller
 
         $user = new \Zank\Model\User;
         $user->phone = $phone;
-        $user->username = sprintf('手机用户_%s', $phone);
+        $user->username = sprintf('用户_%s', $phone);
         $user->hash = str_random(64);
         $user->password = md5($user->hash.$password);
 
-        var_dump($user);
-        exit;
+        if ($user->save()) {
+
+            return $this->in($request, $response);
+        }
+
+        $response = new \Zank\Common\Message($response, false, '注册失败！');
+
+        return $response->withJson();
     }
 
     /**
@@ -91,7 +95,8 @@ class Sign extends Controller
     public function __invoke(Request $request, Response $response)
     {
         $response->withJson([
-            '/api/sign/up' => '用户注册',
+            '/api/sign/in' => '用户登陆',
+            '/api/sign/up/setp/base' => '用户基本信息注册',
         ]);
 
         return $response;
