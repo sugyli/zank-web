64eee2b864dbb0b61f1508ad37951f5d91af3b09
Seven <lovevipdsw@outlook.com>
2016-08-26 09:29:00 +0800

    添加手机验证码数据表迁移器

---
+++ src/db/tables/captcha_phone.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+
+return function (Blueprint $table): void
+{
+    $table->increments('captcha_phone_id');
+    $table->string('phone', 100);
+    $table->string('captcha_code', 50);
+    $table->integer('expires')->nullable()->default(3600);
+    $table->timestamps();
+    $table->softDeletes();
+
+    $table->index('phone');
+};
