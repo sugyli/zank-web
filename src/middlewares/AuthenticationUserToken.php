3685088140e488b331d5299f921104da3fe018db
Seven <lovevipdsw@outlook.com>
2016-08-27 10:44:12 +0800

    完成整个认证过程，并注入user

--- src/middlewares/AuthenticationUserToken.php
+++ src/middlewares/AuthenticationUserToken.php
@@ -5,6 +5,7 @@ namespace Zank\Middleware;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Interop\Container\ContainerInterface;
+use Carbon\Carbon;
 
 /**
  * 用户token认证（用于API请求）
@@ -25,17 +26,28 @@ class AuthenticationUserToken
         $token = $request->getHeaderLine('zank-token');
         $token = \Zank\Model\SignToken::byToken($token)->first();
 
-        var_dump($token);exit;
-
         if (!$token) {
 
-            return with(new \Zank\Common\Message($response, false, '认证失败或者认证信息不存在。'))
+            return with(new \Zank\Common\Message($response, false, '认证失败或者认证信息不存在。', -1))
+                ->withJson()
+            ;
+
+        // 是否过期
+        } elseif ($token->updated_at->diffInSeconds(Carbon::now()) >= (60 * 60 * 24)) {
+
+            return with(new \Zank\Common\Message($response, false, '登陆过期', -2))
+                ->withJson()
+            ;
+
+        // 查询注入的用户是否存在
+        } elseif (!$token->user) {
+            
+            return with(new \Zank\Common\Message($response, false, '认证用户不存在！', -3))
                 ->withJson()
             ;
         }
 
-        var_dump($token);
-        exit;
+        $this->ci->offsetSet('user', $token->user);
 
         return $next($request, $response);
     }
