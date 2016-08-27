4123d363d5ebfc532d2c87fec24f02d3e4df3108
Seven <lovevipdsw@outlook.com>
2016-08-27 10:43:43 +0800

    修正字段名称

--- src/db/tables/user_invite.php
+++ src/db/tables/user_invite.php
@@ -6,12 +6,12 @@ return function (Blueprint $table): void
 {
     $table->increments('user_invite_id');
     $table->string('invite_code', 255);
-    $table->integer('create_users_id');
-    $table->integer('use_users_id')->nullable()->default(null);
+    $table->integer('create_user_id');
+    $table->integer('use_user_id')->nullable()->default(null);
     $table->timestamps();
     $table->softDeletes();
 
     $table->index('invite_code');
-    $table->index('create_users_id');
-    $table->index('use_users_id');
+    $table->index('create_user_id');
+    $table->index('use_user_id');
 };
