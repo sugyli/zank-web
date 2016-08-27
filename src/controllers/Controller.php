2e29693c336940b91939f0ecbb420a5b29163247
Seven <lovevipdsw@outlook.com>
2016-08-27 11:03:57 +0800

    添加基类controller构造方法注释

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -14,6 +14,13 @@ abstract class Controller
 {
     protected $ci;
 
+    /**
+     * 构造方法，注入ci
+     *
+     * @param ContainerInterface $ci 注入器
+     * @author Seven Du <lovevipdsw@outlook.com>
+     * @homepage http://medz.cn
+     */
     public function __construct(ContainerInterface $ci) {
         $this->ci = $ci;
     }
