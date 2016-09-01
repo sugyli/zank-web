ab70311e3149babf694b8d41e65002fcaa4c3e6e
Seven <lovevipdsw@outlook.com>
2016-09-01 15:38:49 +0800

    添加新的应用文件

---
+++ src/App.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zank;
+
+use Interop\Container\ContainerInterface;
+use Slim\App as SlimApp;
+
+/**
+ * Zank application.
+ *
+ * @author Seven Du <lovevipdsw@outlook.com>
+ **/
+class App extends SlimApp   
+{
+    protected static $application;
+
+    public function setAsGlobal()
+    {
+        self::$application = &$this;
+    }
+
+    public static function getApplication()
+    {
+        return self::$application;
+    }
+} // END class App extends SlimApp  
