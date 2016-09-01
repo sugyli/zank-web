186484cfa134847f0661d040e01e35267e05b345
Seven <lovevipdsw@outlook.com>
2016-09-01 15:40:47 +0800

    配置修改

--- src/settings.php
+++ src/settings.php
@@ -2,54 +2,58 @@
 
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'displayErrorDetails' => env('DISPLAY_ERROR_DETAILS', true), // set to false in production
+        'addContentLengthHeader' => env('ADD_CONTENT_LENGTH_HEADER', false), // Allow the web server to send the content-length header
+
         // Monolog settings
         'logger' => [
-            'name' => 'zank',
-            'level' => Monolog\Logger::DEBUG,
-            'path' => __DIR__.'/../logs/app.log',
+            'name' => env('LOGGER_NAME', 'zank'),
+            'level' => env('LOGGER_LEVEL', Monolog\Logger::DEBUG),
+            'path' => dirname(__DIR__).env('LOGGER_PATH', '/logs/app.log'),
         ],
+
         // Eloquent settings
         'db' => [
-            'default' => 'pgsql',
+            'default' => env('DB_DRIVER', 'pgsql'),
 
             'connections' => [
 
                 'sqlite' => [
                     'driver' => 'sqlite',
-                    'database' => 'database.sqlite',
-                    'prefix' => 'medz',
+                    'database' => env('DB_DATABASE', 'database.sqlite'),
+                    'prefix' => env('DB_PREFIX', 'medz_'),
                 ],
 
                 'mysql' => [
                     'driver' => 'mysql',
-                    'host' => 'localhost',
-                    'port' => '3306',
-                    'database' => 'zank',
-                    'username' => 'root',
-                    'password' => '',
-                    'charset' => 'utf8',
-                    'collation' => 'utf8_unicode_ci',
-                    'prefix' => 'medz_',
-                    'strict' => false,
-                    'engine' => null,
+                    'host' => env('DB_HOST', 'localhost'),
+                    'port' => env('DB_PORT', 3306),
+                    'database' => env('DB_DATABASE', 'zank'),
+                    'username' => env('DB_USERNAME', 'root'),
+                    'password' => env('DB_PASSWORD', ''),
+                    'charset' => env('DB_CHARSET', 'utf8'),
+                    'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+                    'prefix' => env('DB_PREFIX', 'medz_'),
+                    'strict' => env('DB_STRICT', false),
+                    'engine' => ENV('DB_ENGINE', null),
                 ],
 
                 'pgsql' => [
                     'driver' => 'pgsql',
-                    'host' => 'localhost',
-                    'port' => '5432',
-                    'database' => 'zank',
-                    'username' => 'zank',
-                    'password' => '',
-                    'charset' => 'utf8',
-                    'prefix' => 'medz_',
-                    'schema' => 'public',
+                    'host' => env('DB_HOST', 'localhost'),
+                    'port' => env('DB_PORT', 5432),
+                    'database' => env('DB_DATABASE', 'zank'),
+                    'username' => env('DB_USERNAME', 'root'),
+                    'password' => env('DB_PASSWORD', ''),
+                    'charset' => env('DB_CHARSET', 'utf8'),
+                    'prefix' => env('DB_PREFIX', 'medz_'),
+                    'schema' => env('DB_SCHEMA', 'public'),
                 ],
 
             ],
 
         ],
+
+        'send:phone:captcha:space' => env('SEND_PHONE_CAPTCHA_SPACE', 300), // 发送验证码间隔时间 s
     ],
 ];
