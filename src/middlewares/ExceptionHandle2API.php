76af40438e70975be4246c53ef0070c286dca4bb
Seven <lovevipdsw@outlook.com>
2016-09-03 21:41:56 +0800

    api异常收集中间件。

---
+++ src/middlewares/ExceptionHandle2API.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Zank\Middleware;
+
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+/**
+ * 错误消息收集处理 针对API.
+ *
+ * @author Seven Du <lovevipdsw@outlook.com>
+ **/
+class ExceptionHandle2API
+{
+
+    /**
+     * ExceptionHandle middleware invokable class.
+     *
+     * @param \Psr\Http\Message\RequestInterface  $request  PSR7 request
+     * @param \Psr\Http\Message\ResponseInterface $response PSR7 response
+     * @param callable                            $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     *
+     * @author Seven Du <lovevipdsw@outlook.com>
+     **/
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $message = null;
+        try {
+            $message = $next($request, $response);
+        } catch (\Exception $e) {
+            $message = $e;
+        } catch (\OSS\OssException $e) {
+            $message = $e;
+        } catch (\Illuminate\Database\QueryException $e) {
+            $message = $e;
+        }
+
+        if ($message instanceof \Exception) {
+            return with(new \Zank\Common\Message($response, false, $message->getMessage()))
+                ->withJson();
+        }
+
+        return $message;
+    }
+} // END class ExceptionHandle
