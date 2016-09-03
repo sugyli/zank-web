a580c5a110d69e442361dd8207e9598dd5abc929
Seven <lovevipdsw@outlook.com>
2016-09-04 02:44:39 +0800

    修改设置配置

--- src/settings.php
+++ src/settings.php
@@ -1,70 +1,73 @@
 <?php
 
 return [
+    
+    'debug' => cfg('DEBUG', true),
+
     'settings' => [
-        'displayErrorDetails'    => env('DISPLAY_ERROR_DETAILS', true), // set to false in production
-        'addContentLengthHeader' => env('ADD_CONTENT_LENGTH_HEADER', false), // Allow the web server to send the content-length header
+        'displayErrorDetails'    => cfg('DISPLAY_ERROR_DETAILS', true), // set to false in production
+        'addContentLengthHeader' => cfg('ADD_CONTENT_LENGTH_HEADER', false), // Allow the web server to send the content-length header
 
         // Monolog settings
         'logger' => [
-            'name'  => env('LOGGER_NAME', 'zank'),
-            'level' => env('LOGGER_LEVEL', Monolog\Logger::DEBUG),
-            'path'  => dirname(__DIR__).env('LOGGER_PATH', '/logs/app.log'),
+            'name'  => cfg('LOGGER_NAME', 'zank'),
+            'level' => cfg('LOGGER_LEVEL', Monolog\Logger::DEBUG),
+            'path'  => dirname(__DIR__).cfg('LOGGER_PATH', '/logs/app.log'),
         ],
 
         // Eloquent settings
         'db' => [
-            'default' => env('DB_DRIVER', 'pgsql'),
+            'default' => cfg('DB_DRIVER', 'pgsql'),
 
             'connections' => [
 
                 'sqlite' => [
                     'driver'   => 'sqlite',
-                    'database' => env('DB_DATABASE', 'database.sqlite'),
-                    'prefix'   => env('DB_PREFIX', 'medz_'),
+                    'database' => cfg('DB_DATABASE', 'database.sqlite'),
+                    'prefix'   => cfg('DB_PREFIX', 'medz_'),
                 ],
 
                 'mysql' => [
                     'driver'    => 'mysql',
-                    'host'      => env('DB_HOST', 'localhost'),
-                    'port'      => env('DB_PORT', 3306),
-                    'database'  => env('DB_DATABASE', 'zank'),
-                    'username'  => env('DB_USERNAME', 'root'),
-                    'password'  => env('DB_PASSWORD', ''),
-                    'charset'   => env('DB_CHARSET', 'utf8'),
-                    'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-                    'prefix'    => env('DB_PREFIX', 'medz_'),
-                    'strict'    => env('DB_STRICT', false),
-                    'engine'    => env('DB_ENGINE', null),
+                    'host'      => cfg('DB_HOST', 'localhost'),
+                    'port'      => cfg('DB_PORT', 3306),
+                    'database'  => cfg('DB_DATABASE', 'zank'),
+                    'username'  => cfg('DB_USERNAME', 'root'),
+                    'password'  => cfg('DB_PASSWORD', ''),
+                    'charset'   => cfg('DB_CHARSET', 'utf8'),
+                    'collation' => cfg('DB_COLLATION', 'utf8_unicode_ci'),
+                    'prefix'    => cfg('DB_PREFIX', 'medz_'),
+                    'strict'    => cfg('DB_STRICT', false),
+                    'engine'    => cfg('DB_ENGINE', null),
                 ],
 
                 'pgsql' => [
                     'driver'   => 'pgsql',
-                    'host'     => env('DB_HOST', 'localhost'),
-                    'port'     => env('DB_PORT', 5432),
-                    'database' => env('DB_DATABASE', 'zank'),
-                    'username' => env('DB_USERNAME', 'root'),
-                    'password' => env('DB_PASSWORD', ''),
-                    'charset'  => env('DB_CHARSET', 'utf8'),
-                    'prefix'   => env('DB_PREFIX', 'medz_'),
-                    'schema'   => env('DB_SCHEMA', 'public'),
+                    'host'     => cfg('DB_HOST', 'localhost'),
+                    'port'     => cfg('DB_PORT', 5432),
+                    'database' => cfg('DB_DATABASE', 'zank'),
+                    'username' => cfg('DB_USERNAME', 'root'),
+                    'password' => cfg('DB_PASSWORD', ''),
+                    'charset'  => cfg('DB_CHARSET', 'utf8'),
+                    'prefix'   => cfg('DB_PREFIX', 'medz_'),
+                    'schema'   => cfg('DB_SCHEMA', 'public'),
                 ],
 
             ],
 
         ],
 
-        'send:phone:captcha:space' => env('SEND_PHONE_CAPTCHA_SPACE', 300), // 发送验证码间隔时间 s
+        'send:phone:captcha:space' => cfg('SEND_PHONE_CAPTCHA_SPACE', 300), // 发送验证码间隔时间 s
 
         // Aliyun OSS settings
         'oss' => [
-            'accessKeyId'     => env('OSS_ACCESS_KEY_ID', ''),
-            'accessKeySecret' => env('OSS_ACCESS_KEY_SECRET', ''),
-            'endpoint'        => env('OSS_ENDPOINT', 'oss-cn-hangzhou.aliyuncs.com'),
-            'bucket'          => env('OSS_BUCKET', ''),
-            'source_url'      => env('OSS_SOURCE_URL', ''),
-            'sign'            => env('OSS_SOURCE_SIGN', false),
-            'timeout'         => env('OSS_SIGN_TIMEOUT', 1800),
+            'accessKeyId'     => cfg('OSS_ACCESS_KEY_ID', ''),
+            'accessKeySecret' => cfg('OSS_ACCESS_KEY_SECRET', ''),
+            'endpoint'        => cfg('OSS_ENDPOINT', 'oss-cn-hangzhou.aliyuncs.com'),
+            'bucket'          => cfg('OSS_BUCKET', ''),
+            'source_url'      => cfg('OSS_SOURCE_URL', ''),
+            'sign'            => cfg('OSS_SOURCE_SIGN', false),
+            'timeout'         => cfg('OSS_SIGN_TIMEOUT', 1800),
         ],
     ],
 ];
