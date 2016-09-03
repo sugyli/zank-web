f800bf15f011083096e1aafb4c2095966c9397fd
Seven <lovevipdsw@outlook.com>
2016-09-03 21:42:01 +0800

    附件数据模型

---
+++ src/models/Attach.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zank\Model;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * 附件模型.
+ *
+ * @author Seven Du<lovevipdsw@outlook.com>
+ **/
+class Attach extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'attachs';
+
+    protected $primaryKey = 'attach_id';
+
+    public function scopeByMd5(Builder $query, $md5)
+    {
+        return $query->where('md5', $md5);
+    }
+} // END class Attach extends Eloquent
