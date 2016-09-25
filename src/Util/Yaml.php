d8fef50eafa491969cefce5c2131468ef6df9f88
Seven <lovevipdsw@outlook.com>
2016-09-25 15:07:01 +0800

    添加应用yaml配置解析类，支持子成员获取。

---
+++ src/Util/Yaml.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Zank\Util;
+
+use Symfony\Component\Yaml\Yaml as BaseYaml;
+
+class Yaml extends BaseYaml
+{
+    public $cfg = [];
+
+    public static $clients = [];
+
+    private function __construct(string $filename)
+    {
+        if (!file_exists($filename) || !is_file($filename)) {
+            throw new \Exception(sprintf('file:%s not find.', $filename));
+        }
+
+        $content = file_get_contents($filename);
+        $this->cfg = self::parse($content);
+    }
+
+    public function get($key, $default = null)
+    {
+        if (!isset($this->cfg[$key])) {
+            return $default;
+        }
+
+        return $this->cfg[$key];
+    }
+
+    public static function addClient($key, string $filename)
+    {
+        if (!isset(self::$clients[$key])) {
+            self::$clients[$key] = new self($filename);
+        }
+    }
+
+    public static function getClient($key)
+    {
+        if (isset(self::$clients[$key])) {
+            return self::$clients[$key];
+        }
+
+        return null;
+    }
+}
