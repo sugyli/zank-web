ee7e0bd93a37af011c228fedd40d2751b0f275f2
Seven <lovevipdsw@outlook.com>
2016-09-25 15:05:52 +0800

    增加cli应用类

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -2,6 +2,21 @@
 
 namespace Zank\Console;
 
+use Symfony\Component\Console\Application as BaseApplication;
+
 class Application extends BaseApplication
 {
+    public static $name = 'Zank Command Tool';
+    public static $version = '1.0.0';
+
+    public function __construct()
+    {
+        error_reporting(-1);
+
+        parent::__construct(self::$name, self::$version);
+
+        $this->add(new Command\TableImportCommand());
+        $this->add(new Command\TableOutCommand());
+        $this->setDefaultCommand('list');
+    }
 }
