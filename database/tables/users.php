c286b32913b6237b3c07da5973709b96dad48a05
Seven <lovevipdsw@outlook.com>
2016-10-02 10:27:19 +0800

    添加一些注释。加入情感状态。

--- database/tables/users.php
+++ database/tables/users.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Schema\Blueprint;
 
 return function (Blueprint $table) {
     $table->increments('user_id');
-    $table->string('phone');
-    $table->string('username', 100);
-    $table->string('password', 255);
-    $table->string('hash', 100);
-    $table->bigInteger('avatar')->nullable()->default(0);
-    $table->smallInteger('age')->nullable()->default(0);
-    $table->smallInteger('height')->nullable()->default(170);
-    $table->smallInteger('kg')->nullable()->default(60);
-    $table->integer('areas_id')->nullable()->default(0);
-    $table->enum('role', ['1', '0.5', '0', '-1'])->nullable()->default('0.5');
-    $table->enum('shape', ['壮熊', '狒狒', '肌肉', '普通', '偏瘦']);
+    $table->string('phone'); // 手机号
+    $table->string('username', 100); // 用户名
+    $table->string('password', 255); // 密码
+    $table->string('hash', 100); // 盐值
+    $table->bigInteger('avatar')->nullable()->default(0); // 头像附件id
+    $table->smallInteger('age')->nullable()->default(0); // 年龄
+    $table->smallInteger('height')->nullable()->default(170); // 身高
+    $table->smallInteger('kg')->nullable()->default(60); // 体重
+    $table->integer('areas_id')->nullable()->default(0); // 地区id
+    $table->enum('role', ['1', '0.5', '0', '-1'])->nullable()->default('0.5'); // 角色
+    $table->enum('shape', ['壮熊', '狒狒', '肌肉', '普通', '偏瘦']); // 体型
+    $table->smallInteger('love', 1)->nullable()->default(0); // 情感状态
     $table->string('geohash', 100)->nullable()->default(null);
     $table->string('latitude', 100)->nullable()->default(null);
     $table->string('longitude', 100)->nullable()->default(null);
