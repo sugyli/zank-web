d24790aaf53e0ba708ca5ba8661dd779e9bc0dbb
Seven <lovevipdsw@outlook.com>
2016-10-03 10:19:22 +0800

    增加修改头像中间件。

---
+++ src/Middleware/User/Change/Username.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zank\Middleware\User\Change;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Zank\Trait\Container;
+
+class Username
+{
+    use Container;
+
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        var_dump($this->ci);
+        exit;
+    }
+}
