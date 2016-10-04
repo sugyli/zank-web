7f3eeb562d07bf6772b371dee3fa0770852c04f8
Seven <lovevipdsw@outlook.com>
2016-10-04 18:40:48 +0800

    添加修改身高的中间件。

---
+++ src/Middleware/User/Change/Height.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zank\Middleware\User\Change;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Zank\Traits\Container;
+
+class Height
+{
+    use Container;
+
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $height = (int) $request->getParsedBodyParam('height');
+        $user = $this->ci->get('user');
+
+        if ($height) {
+
+            if ($height < 20 || $height > 320) {
+                return with(new \Zank\Common\Message($response, false, '不合法的身高范围'))
+                    ->withJson();
+            }
+
+            $user->height = $height;
+        }
+
+        return $next($request, $response);
+    }
+}
