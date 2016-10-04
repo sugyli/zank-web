8de8d3078f4a82ef1c6fd7c91dacf0b782b28921
Seven <lovevipdsw@outlook.com>
2016-10-04 17:03:44 +0800

    完成修改用户名的中间件。

--- src/Middleware/User/Change/Username.php
+++ src/Middleware/User/Change/Username.php
@@ -5,6 +5,7 @@ namespace Zank\Middleware\User\Change;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Zank\Traits\Container;
+use Zank\Model;
 
 class Username
 {
@@ -12,7 +13,30 @@ class Username
 
     public function __invoke(Request $request, Response $response, callable $next)
     {
-        var_dump($this->ci);
-        exit;
+        $user = $this->ci->get('user');
+        $username = $request->getParsedBodyParam('username');
+
+        if ($username) {
+            if ($this->chackUsernameForMe($username) === false) {
+                return with(new \Zank\Common\Message($response, false, '该用户名不可用！'))
+                ->withJson();
+            }
+
+            $user->username = $username;
+        }
+
+        return $next($request, $response);
+    }
+
+    protected function chackUsernameForMe(string $username): bool
+    {
+        $user_id = $this->ci->get('user')->user_id;
+        $old_username = $this->ci->get('user')->username;
+
+        if ($old_username != $username && !($user = Model\User::byUserName($username)->first())) {
+            return true;
+        }
+
+        return $user_id === $user->user_id;
     }
 }
