935dce4a0cd699301884b6e34c5176d0f725003c
Seven <lovevipdsw@outlook.com>
2016-10-05 20:12:19 +0800

    添加地区修改中间件。

---
+++ src/Middleware/User/Change/Area.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zank\Middleware\User\Change;
+
+use Psr\Http\RequestInterface as Request;
+use Psr\Http\RequestInterface as Response;
+use Zank\Traits\Container;
+use Zank\Model;
+
+/**
+ * 修改用户信息，地区修改中间件。
+ *
+ * @author Seven Du <lovevipdsw@outlook.com> 
+ **/
+class Area
+{
+    use Container;
+
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $user = $this->ci->get('user');
+        $area_id = $request->getParsedBodyParam('area_id');
+
+        if ($area_id && ($area = Model\Area::find($area_id))) {
+            # code...
+        }
+    }
+} // END class Area
