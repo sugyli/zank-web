cbf49c551e8321d1b36d4f121521a40be6a6c312
Seven <lovevipdsw@outlook.com>
2016-10-05 20:58:20 +0800

    添加地区模型

---
+++ src/Model/Area.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zank\Model;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Area extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'areas';
+
+    protected $primaryKey = 'area_id';
+}
