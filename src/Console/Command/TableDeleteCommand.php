18d3248a63b888d290d6fa0e3af7a6d2aeb284a3
Seven <lovevipdsw@outlook.com>
2016-10-06 14:49:56 +0800

    重构删除数据库命令

--- src/Console/Command/TableDeleteCommand.php
+++ src/Console/Command/TableDeleteCommand.php
@@ -4,43 +4,76 @@ namespace Zank\Console\Command;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
+use Zank\Util\DatabaseTablesIterator;
+use Zank\Traits\InitDatabaseToConsole;
 
-class TableOutCommand extends Command
+class TableDeleteCommand extends Command
 {
+    use InitDatabaseToConsole;
+
     protected function configure()
     {
         $this
             ->setName('db:delete')
+            ->addOption('y', 'y', InputOption::VALUE_NONE, '忽略删除警告询问.')
+            ->addOption('confirm', null, InputOption::VALUE_OPTIONAL, '设置询问消息.', '删除数据库表结构会连同数据一起删除，是否确定删除？')
+            ->addOption('confirm-no-message', null, InputOption::VALUE_OPTIONAL, '设置取消命令输出消息.', '已经取消删除数据表结构.')
+            ->addOption('no-title', null, InputOption::VALUE_NONE, '不显示命令标题.')
             ->setDescription('Delete all tables.')
             ->setHelp('This command delete all tables to database.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln([
-            'Delete all tables.',
-            '======================',
-            '',
-        ]);
+        $io = new SymfonyStyle($input, $output);
+        $input->getOption('no-title') === false && $io->title('删除数据库中所有的表结构和数据');
 
-        // $tablesDir = dirname(__DIR__).'/db/tables';
-        // $finder = new Finder();
-        // $finder
-        //     ->files()
-        //     ->in($tablesDir)
-        //     ->name('*.php');
+        $io->note('建议先对数据库进行备份。');
+        if ($input->getOption('y') === false) {
+            $confirm = $io->confirm($input->getOption('confirm'), false);
+            if ($confirm === false) {
+                $output->writeln(
+                    sprintf(
+                        '<question>%s</question>',
+                        $input->getOption('confirm-no-message')
+                    )
+                );
 
-        // $i = 0;
-        // foreach ($finder as $file) {
-        //     $tableName = $file->getBasename('.php');
-        //     Capsule::Schema()->dropIfExists($tableName); // 删除数据库的表
-        //     $output->writeln('Delete table:'.$tableName.' <fg=green>OK.</>');
-        //     $i++;
-        // }
+                return -1;
+            }
+        }
 
-        // $output->writeln(sprintf('<info>Delete table num:%d</info>', $i));
+        $files = new DatabaseTablesIterator;
+        $io->progressStart($files->count());
+        $tables = [];
+
+        $files->forEach(function ($file) use (&$tables, $io) {
+            $tableName = $file->getBasename('.php');
+
+            try {
+                Capsule::Schema()->dropIfExists($tableName); // 删除数据库的表
+            } catch (\Exception $e) {
+                $tables[] = [$tableName, $e->getMessage()];
+            }
+
+            $io->progressAdvance();
+        });
+
+        $io->progressFinish();
+        $this->showTables($tables, $io);
+    }
+
+    protected function showTables(array $tables = [], SymfonyStyle $io)
+    {
+        if (count($tables) > 0) {
+            $io->table(
+                ['表名 [不含前缀]', '错误消息'],
+                $tables
+            );
+        }
     }
 }
