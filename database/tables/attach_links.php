85dacea21763fbe0b8feb262cbafc8e87b31b053
Seven <lovevipdsw@outlook.com>
2016-10-07 20:12:25 +0800

    重新设计附件中间表

--- database/tables/attach_links.php
+++ database/tables/attach_links.php
@@ -3,10 +3,9 @@
 use Illuminate\Database\Schema\Blueprint;
 
 return function (Blueprint $table) {
+    $table->increments('attach_link_id');
     $table->integer('user_id');
     $table->integer('attach_id');
-    $table->timestamps();
-    $table->softDeletes();
     $table->index('user_id');
     $table->index('attach_id');
 };
