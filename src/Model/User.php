c4b14081307fb2276130863747d4abfbf69d3953
Seven <lovevipdsw@outlook.com>
2016-10-08 10:31:40 +0800

    添加注释。

--- src/Model/User.php
+++ src/Model/User.php
@@ -37,11 +37,26 @@ class User extends Model
             ->where('phone', $phone);
     }
 
+    /**
+     * 用于查询username字段
+     *
+     * @param Builder $query 查询构造器
+     * @param string $username 用户名
+     * @return Buolder
+     * @author Seven Du <lovevipdsw@outlook.com>
+     * @homepage http://medz.cn
+     */
     public function scopeByUserName(Builder $query, $username): Builder
     {
         return $query->where('username', $username);
     }
 
+    /**
+     * 用户下的附件关联模型
+     *
+     * @author Seven Du <lovevipdsw@outlook.com>
+     * @homepage http://medz.cn
+     */
     public function attachs()
     {
         return $this->belongsToMany(Attach::class, 'attach_links', 'user_id', 'attach_id');
