c66406531bac616845f80ec247187d5ef3237ffe
Seven <lovevipdsw@outlook.com>
2016-10-09 22:59:06 +0800

    支持指定表导入到数据库。

--- src/Console/Command/TableImportCommand.php
+++ src/Console/Command/TableImportCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,6 +21,7 @@ class TableImportCommand extends Command
     {
         $this
             ->setName('db:import')
+            ->addArgument('table', InputArgument::IS_ARRAY, '需要导入的表名称(用空格分隔多个名称).', [])
             ->addOption('y', 'y', InputOption::VALUE_NONE, '忽略删除警告询问.')
             ->setDescription('Import tables to database.')
             ->setHelp('This command allows you to import tables to database.');
@@ -28,7 +30,13 @@ class TableImportCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $io->title('(setp:1) 删除数据库中的表结构和数据');
+        $io->title('将数据结构导入到数据库.');
+        $io->newLine(1);
+
+        $tableNames = $input->getArgument('table');
+        $files = new DatabaseTablesIterator($tableNames);
+
+        $io->section('(setp:1) 删除数据库中的表结构和数据');
 
         $commandName = 'db:delet';
         $command = $this->getApplication()->find($commandName);
@@ -37,6 +45,7 @@ class TableImportCommand extends Command
             '--confirm' => '导入数据表会删除已有的数据表和数据,是否确定删除并导入？',
             '--confirm-no-message' => '已经取消导入数据表结构.',
             '--no-title' => true,
+            'table' => $tableNames,
         ];
 
         $input->getOption('y') === true && $arguments['--y'] = true;
@@ -48,10 +57,9 @@ class TableImportCommand extends Command
             return 1;
         }
 
-        $io->title('(setp:2) 导入数据表结构到数据库中');
+        $io->section('(setp:2) 导入数据表结构到数据库中');
 
         $tables = [];
-        $files = new DatabaseTablesIterator;
         $io->progressStart($files->count());
 
         $files->forEach(function ($file) use (&$tables, $io) {
