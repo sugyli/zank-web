a2188b32a88b257ab5b06a60ac0b543d45b11977
Seven <lovevipdsw@outlook.com>
2016-10-09 22:58:55 +0800

    删除表支持自定义表删除，否则为全部。

--- src/Console/Command/TableDeleteCommand.php
+++ src/Console/Command/TableDeleteCommand.php
@@ -5,6 +5,7 @@ namespace Zank\Console\Command;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,6 +20,7 @@ class TableDeleteCommand extends Command
     {
         $this
             ->setName('db:delete')
+            ->addArgument('table', InputArgument::IS_ARRAY, '需要数据表名称(用空格分隔多个名称).', [])
             ->addOption('y', 'y', InputOption::VALUE_NONE, '忽略删除警告询问.')
             ->addOption('confirm', null, InputOption::VALUE_OPTIONAL, '设置询问消息.', '删除数据库表结构会连同数据一起删除，是否确定删除？')
             ->addOption('confirm-no-message', null, InputOption::VALUE_OPTIONAL, '设置取消命令输出消息.', '已经取消删除数据表结构.')
@@ -30,9 +32,12 @@ class TableDeleteCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
+        $tableNames = $input->getArgument('table');
+        $files = new DatabaseTablesIterator($tableNames);
+
         $input->getOption('no-title') === false && $io->title('删除数据库中所有的表结构和数据');
 
-        $io->note('建议先对数据库进行备份。');
+        $io->note('建议先对数据库进行备份.');
         if ($input->getOption('y') === false) {
             $confirm = (bool) $io->confirm($input->getOption('confirm'), false);
             if ($confirm === false) {
@@ -47,7 +52,6 @@ class TableDeleteCommand extends Command
             }
         }
 
-        $files = new DatabaseTablesIterator;
         $io->progressStart($files->count());
         $tables = [];
 
