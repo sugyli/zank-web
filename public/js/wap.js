227b260e57e87e0fe378c67f5865712d1b43862b
sugyli <3101967255@qq.com>
2017-01-04 12:32:10 +0800

    1.4.2.5

--- public/js/wap.js
+++ public/js/wap.js
@@ -551,5 +551,27 @@ function _17mb_tj(){//统计代码
       var s = document.getElementsByTagName("script")[0]; 
       s.parentNode.insertBefore(hm, s);
     })();
+
+    //百度推送
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+
+    //360
+    (function(){
+       var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?1dc911826b929068983b46c9a795f5d4":"https://jspassport.ssl.qhimg.com/11.0.1.js?1dc911826b929068983b46c9a795f5d4";
+       document.write('<script src="' + src + '" id="sozz"><\/script>');
+    })();
+
+
 }
 
