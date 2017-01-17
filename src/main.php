2d6f10ae06e743c34cc4b00b9c4d563ccb31f190
sugyli <3101967255@qq.com>
2017-01-17 10:42:01 +0800

    17

--- src/main.php
+++ src/main.php
@@ -11,7 +11,7 @@
     define('TXTDIR', "http://txt.dashubao.co/"); 
     define('IMAGEDIR', "http://img.dashubao.co/"); 
     define('PAGENUM', 40);//目录默认多少分页
-    define('SPRIT', ['dashubao.co','sugyli.com']);//兼容大书包处理/的问题
+    define('SPRIT', ['dashubao.co','sugyli.com','dashubao.net']);//兼容大书包处理/的问题
     define('NOVELMB', 'Novel/Wap/');//小说模板
     define('NOVELWEBMB', 'Novel/Web/');//小说模板
     define('PAGESIZE', 30);//首页加载多少条
